8d1b45289199af96a8d9fe0493c96d38146a3802
standard gitter <4243354+standardgitter@users.noreply.github.com>
2021-06-10 03:55:59 +0800

    Update mysql_test.c

--- mysql_test.c
+++ mysql_test.c
@@ -85,7 +85,7 @@ int main(int argc, char* argv[])
       
         printf("MYSQL VERSON IS : %s\n",mysql_get_server_info(&mysql)); 
         mysql_set_character_set(&mysql, "utf-8"); 
-	mysql_autocommit(&mysql, 0);// 1(on) is default, turn the autocommit on meaning auto begin transaction. 
+	mysql_autocommit(&mysql, 0);// 1(on) is default, turn the autocommit on meaning commit every ddl sql, so it very slow. 
         
         mysql_query(&mysql,"CREATE TABLE t1(c1_i int(11) PRIMARY KEY auto_increment, c2_t varchar(16))engine=innodb");
         
