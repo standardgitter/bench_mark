e6182aa4037c15194e80273eb1556d4a8a9c239a
standard gitter <4243354+standardgitter@users.noreply.github.com>
2021-06-10 14:06:59 +0800

    Update mysql_test.c

--- mysql_test.c
+++ mysql_test.c
@@ -141,7 +141,7 @@ void insert_test_myisam(MYSQL *mysql,size_t rows){
 	mysql_query(mysql,"drop table if exists t1");
 	mysql_query(mysql,"CREATE TABLE t1(c1_i int(11) PRIMARY KEY auto_increment, c2_t varchar(16))engine=myisam");
 	
-	gettimeofday(start, NULL);
+	gettimeofday(&start, NULL);
         
         stmt = mysql_stmt_init(mysql);
         mysql_stmt_prepare(stmt, "INSERT INTO t1(c2_t) VALUES(?)", strlen("INSERT INTO t1(c2_t) VALUES(?)"));
@@ -201,7 +201,7 @@ void insert_test_memory(MYSQL *mysql,size_t rows){
 	mysql_query(mysql,"drop table if exists t1");
 	mysql_query(mysql,"CREATE TABLE t1(c1_i int(11) PRIMARY KEY auto_increment, c2_t varchar(16))engine=memory");
 	
-	gettimeofday(start, NULL);
+	gettimeofday(&start, NULL);
         
         stmt = mysql_stmt_init(mysql);
         mysql_stmt_prepare(stmt, "INSERT INTO t1(c2_t) VALUES(?)", strlen("INSERT INTO t1(c2_t) VALUES(?)"));
